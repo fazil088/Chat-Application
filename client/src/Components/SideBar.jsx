7bc516026252a5ad4a352e4ec25facd0dd5aa13f
Muhammed Fazil <mhdf088@gmail.com>
2024-01-15 10:46:37 +0530

    App side bar header and search box are created

--- client/src/Components/SideBar.jsx
+++ client/src/Components/SideBar.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import './styles.css'
+import {IconButton} from '@mui/material'
+import {AccountCircle,PersonAddAlt1,GroupAdd,AddCircle,DarkMode,Search} from '@mui/icons-material'
 
 function SideBar() {
   return (
-    <div className='side-bar'>SideBar</div>
+    <div className='side-bar'>
+      <div className='sb-header'>
+        <div>
+          <IconButton>
+            <AccountCircle/>
+          </IconButton>
+        </div>
+        <div>
+          <IconButton>
+            <PersonAddAlt1/>
+          </IconButton>
+          <IconButton>
+            <GroupAdd/>
+          </IconButton>
+          <IconButton>
+            <AddCircle/>
+          </IconButton>
+          <IconButton>
+            <DarkMode/>
+          </IconButton>
+        </div>
+      </div>
+      <div className='sb-search'>
+        <IconButton>
+          <Search/>
+        </IconButton>
+        <input type="text" className='search-box' />
+      </div>
+      <div className='sb-conversation'>conversation</div>
+    </div>
   )
 }
 
